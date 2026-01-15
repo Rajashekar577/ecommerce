2d18d1aa3986583d8aad4cdd820487eef9100cc3
Rajashekar577 <132990507+Rajashekar577@users.noreply.github.com>
2026-01-15 17:57:29 +0530

    Update Service.java

--- src/Service.java
+++ src/Service.java
@@ -1,7 +1,7 @@
 public class Service {
 
     public  String method1(){
-        System.out.println("i");
+        System.out.println("j");
         System.out.println("o");
         System.out.println("i ");
          System.out.println("i " + " " + "j");
